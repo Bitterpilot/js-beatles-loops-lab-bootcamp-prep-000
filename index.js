ebfd11eca5609c94f284c7b5ee90636646799cac
Nathan M <nathanminissale@gmail.com>
2018-09-06 10:04:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function theBeatlesPlay(musicians, instruments) {
-  var emptyArray = new array
-  for (var i = 0; i < array.length; i++) {
-    array[i]
+  var newArray = new Array();
+  for (var i = 0; i < musicians.length; i++) {
+    newArray = newArray(musician[i] + " " + " " + instruments[i]);
   }
+  returns newArray
 }
 
 function johnLennonFacts(facts) {
-  
+
 }
 
 function iLoveTheBeatles(n) {
-  
+
 }
